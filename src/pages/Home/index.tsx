9495e1a11702b9098975115f11fd858593549c0a
Marcos Oliveira <marcosgarcia.oliveira02@gmail.com>
2022-08-05 12:55:53 -0300

    style: add notification icon at home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,6 +16,7 @@ import {
   calendar,
   chatbubbles,
   checkmarkCircle,
+  notifications,
   people,
   time,
 } from "ionicons/icons";
@@ -198,6 +199,10 @@ const Home = () => {
                 </IonText>
               </div>
               <div className="flex items-center">
+                <IonIcon
+                  className="w-5 h-5 text-gray-500 mr-5"
+                  src={notifications}
+                />
                 <IonAvatar
                   onClick={() => {
                     document.location.replace(
