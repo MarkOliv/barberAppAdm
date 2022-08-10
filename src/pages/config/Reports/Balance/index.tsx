efb0b21d5bed7d94a57b2d7ce6c88353f7d637af
Marcos Oliveira <marcosgarcia.oliveira02@gmail.com>
2022-08-10 09:32:17 -0300

    feat: add balance of  month systen with graphics

--- src/pages/config/Reports/Balance/index.tsx
+++ src/pages/config/Reports/Balance/index.tsx
@@ -17,13 +17,76 @@ import { calendar, chevronBackOutline } from "ionicons/icons";
 import { useAuth } from "../../../../contexts";
 
 import { Chart } from "react-google-charts";
+import supabase from "../../../../utils/supabase";
 
 const Balance = () => {
+  const [consultDate, setConsultDate] = React.useState<any>();
+  const [allCashFlow, setAllCashFlow] = React.useState<Array<any>>([]);
+  const [consultDateCashFlow, setConsultDateAllCashFlow] = React.useState<
+    Array<any>
+  >([]);
+
+  const [creditCashs, setCreditCashs] = React.useState<Number>();
+  const [debitCashs, setDebitCashs] = React.useState<Number>();
+
   const { sessionUser } = useAuth();
 
   const options = {
     title: "Balanço do mês",
   };
+
+  const getCashFlow = async () => {
+    try {
+      let { data: cashFlow, error } = await supabase
+        .from("cashFlow")
+        .select("*");
+
+      if (cashFlow) {
+        setAllCashFlow(cashFlow);
+      }
+    } catch (error) {}
+  };
+
+  const handleFilterDate = async () => {
+    setConsultDateAllCashFlow([]);
+    try {
+      allCashFlow?.map((cashFlow) => {
+        let date = `${cashFlow?.created_at}`;
+        if (date?.includes(consultDate)) {
+          setConsultDateAllCashFlow((current) => [...current, cashFlow]);
+        }
+      });
+    } catch (error) {}
+  };
+
+  const handleCashs = () => {
+    try {
+      let debitTotalValue = 0;
+      let creditTotalValue = 0;
+
+      consultDateCashFlow.map((cashFlow) => {
+        if (cashFlow?.type === "debit") {
+          debitTotalValue += Number(cashFlow?.total_value);
+        } else {
+          creditTotalValue += Number(cashFlow?.total_value);
+        }
+      });
+      setCreditCashs(Number(creditTotalValue.toFixed(2)));
+      setDebitCashs(Number(debitTotalValue.toFixed(2)));
+    } catch (error) {}
+  };
+
+  React.useEffect(() => {
+    getCashFlow();
+  }, []);
+
+  React.useEffect(() => {
+    handleFilterDate();
+  }, [allCashFlow, consultDate]);
+
+  React.useEffect(() => {
+    handleCashs();
+  }, [consultDateCashFlow]);
 
   return (
     <IonPage>
@@ -41,16 +104,16 @@ const Balance = () => {
               </Link>
 
               <div className="py-10 px-5">
-                <div
-                  className="flex flex-wrap justify-center items-center mt-5 mb-3 p-3 bg-white rounded-3xl shadow h-24"
-                  key={"Especialidades"}
-                >
+                <div className="flex flex-wrap justify-center items-center mt-5 mb-3 p-3 bg-white rounded-3xl shadow h-24">
                   <p>Selecione a data</p>
                   <div className="flex justify-center items-center bg-gray-200 rounded-3xl shadow-md h-10 w-full">
                     <IonInput
                       className="text-gray-500"
                       placeholder="dd/mm/aaaa"
                       type={"month"}
+                      onIonChange={({ detail }) => {
+                        setConsultDate(detail?.value);
+                      }}
                     />
                     <IonIcon className="mr-5 text-gray-500" src={calendar} />
                   </div>
@@ -61,11 +124,10 @@ const Balance = () => {
                     className="mt-5 mb-3 bg-white rounded-3xl shadow shadow-green-500 h-20 flex items-center"
                     lines="none"
                     id="open-modal"
-                    key={"Especialidades"}
                   >
                     <IonLabel className="ml-5">
                       <p>RECEITA</p>
-                      <h2>R$2546,00</h2>
+                      <h2>R${creditCashs}</h2>
                     </IonLabel>
                   </IonItem>
 
@@ -73,11 +135,10 @@ const Balance = () => {
                     className="mt-5 mb-3 bg-white rounded-3xl shadow shadow-red-500 h-20 flex items-center"
                     lines="none"
                     id="open-modal"
-                    key={"Especialidades"}
                   >
                     <IonLabel className="ml-5">
                       <p>DESPESA</p>
-                      <h2>R$1440,00</h2>
+                      <h2>R${debitCashs}</h2>
                     </IonLabel>
                   </IonItem>
                 </div>
@@ -87,8 +148,8 @@ const Balance = () => {
                     chartType="PieChart"
                     data={[
                       ["balanceType", "Value"],
-                      ["Receitas", 2546],
-                      ["Despesas", 1440],
+                      ["Receitas", creditCashs],
+                      ["Despesas", debitCashs],
                     ]}
                     options={options}
                     width="100%"
