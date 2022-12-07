a3c81a04d542b09d8cf4d1be4dc21d856f956c95
MarkOliv <marcosgarcia.oliveira02@gmail.com>
2022-12-07 10:33:49 -0300

    last build v2

--- android/app/src/main/java/hairfashion/Barber/MainActivity.java
+++ android/app/src/main/java/hairfashion/Barber/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package hairfashion.Barber;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'barberApp',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "hairfashion.Barber",
+  appName: "Hairfashion Barber",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
